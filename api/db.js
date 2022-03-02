5274b68b5d0de021fddae4d05cb0cafad0114880
Erik Axelsson <e.axelsson@tutanota.com>
2022-03-02 16:09:18 +0100

    Add .env and database file.

---
+++ api/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function callback() {
+  console.log("MongoDB Connected...");
+});
+
+module.exports = db;
