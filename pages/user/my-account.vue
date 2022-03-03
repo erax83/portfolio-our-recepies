99d0bcc290e3d77e580a9964515c689f8b1ffb1c
Erik Axelsson <e.axelsson@tutanota.com>
2022-03-03 11:35:25 +0100

    Add frontend user pages.

---
+++ pages/user/my-account.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>My Account</h1>
+
+    <hr />
+
+    <div class="card">
+      <div class="card-body">
+        <h4>{{ this.$auth.user.full_name }}</h4>
+        <div><strong>Email:</strong> {{ this.$auth.user.email }}</div>
+        <nuxt-link to="/user/logout">Logout</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: "auth",
+};
+</script>
