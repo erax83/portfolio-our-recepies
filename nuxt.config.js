f071264ac40c78a94438dd76d31465e22fcf7774
Erik Axelsson <e.axelsson@tutanota.com>
2022-03-03 11:11:27 +0100

    Add auth module.

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
+    "@nuxtjs/auth",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -38,8 +39,37 @@ export default {
     baseURL: "/",
   },
 
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          // these are the API endpoints we created in Express
+          login: {
+            url: "/api/users/login",
+            method: "post",
+            propertyName: "token",
+          },
+          logout: true,
+          user: {
+            url: "/api/users/user",
+            method: "get",
+            propertyName: "user",
+          },
+        },
+        tokenRequired: true,
+        tokenType: "Bearer",
+      },
+    },
+    redirect: {
+      login: "/user/login", // User will be redirected to this path if login is required
+      logout: "/", // User will be redirected to this path if after logout, current route is protected
+      home: "/", // User will be redirect to this path after login if accessed login page directly
+    },
+    rewriteRedirects: true,
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  serverMiddleware: ["~/api/index.js"]
+  serverMiddleware: ["~/api/index.js"],
 };
